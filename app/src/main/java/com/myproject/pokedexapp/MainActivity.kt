303f47a95fb5595283306b6aa28dc2d5d882239d
firatyildizhan01 <hanyildizfirat@hotmail.com>
2021-12-13 13:51:28 +0300

    layout files and dependencies are set.

--- app/src/main/java/com/myproject/pokedexapp/MainActivity.kt
+++ app/src/main/java/com/myproject/pokedexapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.myproject.pokedexapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.myproject.pokedexapp.ui.theme.JetpackComposePokedexTheme
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            JetpackComposePokedexTheme {
+
+            }
+        }
     }
 }
